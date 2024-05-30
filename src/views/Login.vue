2e123504879507aa3defb991afec526d88ae215e
Do0oMores <do0omores@outlook.com>
2024-05-30 15:19:17 +0800

    登录界面增加空值判断，注册界面和后端兼容

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,96 +1,104 @@
 <template>
 	<div class="container">
-	  <div class="login-wrapper">
-		<div class="header">登录</div>
-		<div class="form-wrapper">
-		  <form @submit.prevent="fetchData">
-			<input v-model="userName" type="text" name="userName" id="userName" placeholder="账号" class="input-item">
-			<input v-model="userPassword" type="userPassword" name="userPassword" id="userPassword" placeholder="密码" class="input-item">
-			<button class="loginButton" id="loginButton" type="submit">登录</button>
-		  </form>
+		<div class="login-wrapper">
+			<div class="header">登录</div>
+			<div class="form-wrapper">
+				<form @submit.prevent="fetchData">
+					<input v-model="userName" type="text" name="userName" id="userName" placeholder="账号"
+						class="input-item">
+					<input v-model="userPassword" type="userPassword" name="userPassword" id="userPassword"
+						placeholder="密码" class="input-item">
+					<button class="loginButton" id="loginButton" type="submit">登录</button>
+				</form>
+			</div>
+			<div class="msg">
+				<a @click="gotoRegister">没有账号？ 点此注册</a>
+			</div>
 		</div>
-		<div class="msg">
-		  <a @click="gotoRegister">没有账号？ 点此注册</a>
+		<div class="extra-content">
+			<p>欢迎来到我们的网站！</p>
+			<p>这里有更多的内容等待您探索。</p>
 		</div>
-	  </div>
-	  <div class="extra-content">
-  <p>欢迎来到我们的网站！</p>
-  <p>这里有更多的内容等待您探索。</p>
-</div>
 	</div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  import axios from 'axios';
-  import { ElMessage } from 'element-plus';
-  import GlobalVar from "../stores/global";
-  
-  const router = useRouter();
-  const userName = ref('');
-  const userPassword = ref('');
-  
-  const gotoRegister = () => {
-	router.push('/register');
-  };
-  
-  const fetchData = async () => {
-	try {
-		const response = await axios.get(`/api/login`, {
-      params: {
-        userName: userName.value,
-        userPassword: userPassword.value
-      }
-    });
-	  if (response.data.code === 0) {
-		ElMessage.success(response.data.msg);
-		setTimeout(() => {
-		  GlobalVar.username = userName.value;
-		  router.push('/admin');
-		}, 1000);
-	  } else {
-		ElMessage.error(response.data.msg);
-	  }
-	} catch (error) {
-	  console.error(error);
-	}
-  };
-  </script>
-  
-  <style scoped>
+</template>
 
-  * {
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+import { ElMessage } from 'element-plus';
+import GlobalVar from "../stores/global";
+
+const router = useRouter();
+const userName = ref('');
+const userPassword = ref('');
+
+const gotoRegister = () => {
+	router.push('/register');
+};
+
+const fetchData = async () => {
+	//判断不为空
+	if (userName.value && userPassword.value) {
+		try {
+			const response = await axios.get(`/api/login`, {
+				params: {
+					userName: userName.value,
+					userPassword: userPassword.value
+				}
+			});
+			if (response.data.code === 0) {
+				ElMessage.success(response.data.msg);
+				setTimeout(() => {
+					GlobalVar.username = userName.value;
+					router.push('/admin');
+				}, 1000);
+			} else {
+				ElMessage.error(response.data.msg);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	} else {
+		ElMessage.error("账号或者密码错误!")
+	}
+
+};
+</script>
+
+<style scoped>
+* {
 	margin: 0;
 	padding: 0;
-  }
-  
-  html, body {
+}
+
+html,
+body {
 	height: 100%;
 	margin: 0;
 	padding: 0;
-  }
-  
-  .container {
-  background: url("../pic/login.jpg") no-repeat center center fixed;
-  background-size: cover;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding-bottom: 20px;
+}
+
+.container {
+	background: url("../pic/login.jpg") no-repeat center center fixed;
+	background-size: cover;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding-bottom: 20px;
 }
 
 .extra-content {
-  margin-top: 300px;
-  text-align: center;
-  color: white;
+	margin-top: 300px;
+	text-align: center;
+	color: white;
 }
 
-  
-  .login-wrapper {
+
+.login-wrapper {
 	background-color: rgba(255, 255, 255, 0.4);
 	box-shadow: 0 5px 15px rgba(20, 20, 20, 0.8);
 	width: 358px;
@@ -102,16 +110,16 @@
 	justify-content: center;
 	align-items: center;
 	backdrop-filter: blur(3px);
-  }
-  
-  .header {
+}
+
+.header {
 	font-size: 38px;
 	font-weight: bold;
 	text-align: center;
 	margin-bottom: 20px;
-  }
-  
-  .input-item {
+}
+
+.input-item {
 	display: block;
 	width: 100%;
 	margin-bottom: 20px;
@@ -120,33 +128,32 @@
 	border-bottom: 1px solid rgb(128, 125, 125);
 	font-size: 15px;
 	outline: none;
-  }
-  
-  .input-item::placeholder {
+}
+
+.input-item::placeholder {
 	text-transform: uppercase;
-  }
-  
-  .loginButton {
+}
+
+.loginButton {
 	text-align: center;
 	padding: 10px;
 	width: 100%;
 	margin-top: 20px;
 	background-image: linear-gradient(to right, #a6c1ee, #fbc2eb);
 	color: #fff;
-  }
-  
-  .loginButton:hover {
+}
+
+.loginButton:hover {
 	background-image: linear-gradient(to left, #a6c1ee, #fbc2eb);
-  }
-  
-  .msg {
+}
+
+.msg {
 	text-align: center;
 	margin-top: 20px;
-  }
-  
-  a {
+}
+
+a {
 	text-decoration: none;
 	color: #8387cd;
-  }
-  </style>
-  
+}
+</style>
