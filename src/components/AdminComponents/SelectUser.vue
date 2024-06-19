2b9000a5fff8e81ac3614c11d4a4944d54b3e198
Do0oMores <do0omores@outlook.com>
2024-06-19 22:39:19 +0800

    Update
    admin edit user information

--- src/components/AdminComponents/SelectUser.vue
+++ src/components/AdminComponents/SelectUser.vue
@@ -17,11 +17,46 @@
             当前查询的用户是 [{{ username }}] 点击返回重新查询其他用户
         </el-button>
         <el-table :data="tableData" stripe style="width: 100%">
-            <el-table-column prop="id" label="ID" width="180" />
-            <el-table-column prop="name" label="姓名" width="180" />
-            <el-table-column prop="phone" label="电话" />
-            <el-table-column prop="is_admin" label="是否为管理员" />
-            <el-table-column prop="register_date" label="注册日期" />
+            <el-table-column prop="id" label="ID" width="180">
+                <template #default="scope">
+                    <el-input v-if="editIndex === scope.$index" v-model="scope.row.id" placeholder="ID"></el-input>
+                    <span v-else>{{ scope.row.id }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" width="180">
+                <template #default="scope">
+                    <el-input v-if="editIndex === scope.$index" v-model="scope.row.name" placeholder="姓名"></el-input>
+                    <span v-else>{{ scope.row.name }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="phone" label="电话">
+                <template #default="scope">
+                    <el-input v-if="editIndex === scope.$index" v-model="scope.row.phone" placeholder="电话"></el-input>
+                    <span v-else>{{ scope.row.phone }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="is_admin" label="是否为管理员">
+                <template #default="scope">
+                    <el-select v-if="editIndex === scope.$index" v-model="scope.row.is_admin" placeholder="是否为管理员">
+                        <el-option label="是" value="是"></el-option>
+                        <el-option label="否" value="否"></el-option>
+                    </el-select>
+                    <span v-else>{{ scope.row.is_admin }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="register_date" label="注册日期">
+                <template #default="scope">
+                    <el-date-picker v-if="editIndex === scope.$index" v-model="scope.row.register_date" type="date"
+                        placeholder="选择日期"></el-date-picker>
+                    <span v-else>{{ scope.row.register_date }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作">
+                <template #default="scope">
+                    <el-button v-if="editIndex !== scope.$index" @click="editRow(scope.$index)">编辑</el-button>
+                    <el-button v-else type="primary" @click="saveRow(scope.$index)">确认</el-button>
+                </template>
+            </el-table-column>
         </el-table>
     </div>
 </template>
@@ -29,13 +64,23 @@
 <script lang="ts">
 import axios from 'axios';
 import { ElMessage } from "element-plus";
+
+interface TableData {
+    id: string;
+    name: string;
+    phone: string;
+    is_admin: string;
+    register_date: string;
+}
+
 export default {
-    data: function () {
+    data() {
         return {
             isSelected: true,
             username: "",
-            tableData: []
-        }
+            tableData: [] as TableData[],
+            editIndex: -1
+        };
     },
     methods: {
         fetchData() {
@@ -44,7 +89,6 @@ export default {
             // 判断用户名是否为空
             if (this.username != "") {
                 axios.get('/api/current?username=' + this.username).then(response => {
-                    //console.log(response.data);
                     if (response.data.code == 200) {
                         ElMessage.success(response.data.msg);
                         this.tableData = response.data.Data;
@@ -64,6 +108,34 @@ export default {
         },
         back() {
             this.isSelected = true
+        },
+        editRow(index: number) {
+            this.editIndex = index;
+        },
+        saveRow(index: number) {
+            // 获取当前编辑行的数据
+            const userData = this.tableData[index];
+
+            // 发起请求传递编辑后的数据
+            axios.get('/api/save-user', {
+                params: {
+                    userID: userData.id,
+                    name: userData.name,
+                    phone: userData.phone,
+                    isAdmin: userData.is_admin,
+                    registerDate: userData.register_date
+                }
+            }).then(response => {
+                if (response.data.code == 200) {
+                    ElMessage.success(response.data.msg);
+                    this.editIndex = -1; // 退出编辑模式
+                } else {
+                    ElMessage.error(response.data.msg);
+                }
+            }).catch(error => {
+                console.log(error);
+                ElMessage.error("请求失败");
+            });
         }
     }
 }
